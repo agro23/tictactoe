ac7e4f15ec2a950a60ca0ba2c05f7c4ee05611db
Andy Grossberg and Kayla Ondracek <andy@>
2018-01-24 10:11:54 -0800

    add skeleton for initial board space and handle non X/O user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
-function setUser() {
-  var user = prompt("Do you want to be X or O?");
+function getUser() {
+  var user = "";
+  while (user !== "X" && user !== "O") {
+    user = prompt("Do you want to be X or O?").toUpperCase();
+   }
   return user;
 }
 
+function buildSpace() {
+  var spaceTemplate = "<div>_</div>";
+  return spaceTemplate;
+}
+
+function displaySpace(thing) {
+  $(".board").html(thing);
+}
+
 $(document).ready(function() {
-  var userType = setUser();
+  var userType = getUser();
   alert("you chose " + userType + ", great job");
+
+  displaySpace(buildSpace());
 });
