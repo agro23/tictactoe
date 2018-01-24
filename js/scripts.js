423a2dfbfee83ff7dd86de6e2a64a66be5e507c3
Andy Grossberg and Kayla Ondracek <andy@>
2018-01-24 11:10:39 -0800

    build tic tac toe board

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,52 @@ function getUser() {
   return user;
 }
 
-function buildSpace() {
-  var spaceTemplate = "<div>_</div>";
+function buildSpace(coordinate) {
+  var spaceTemplate = "<div id='space" + coordinate + "'>_</div>";
   return spaceTemplate;
 }
 
-function displaySpace(thing) {
-  $(".board").html(thing);
+// function displaySpace(thing) {
+//   $(".board").html(thing);
+// }
+
+function Board(spaces) {
+  this.spaces = spaces; //array of spaces
 }
+
+Board.prototype.buildBoard = function() {
+  for (var i = 1; i < 10; i++) {
+    this.spaces.push(buildSpace(i));
+  }
+  return this.spaces;
+}
+
+function placeInSpace(thing, coordinate) {
+  $("#space" + coordinate).text(thing);
+}
+
+function displayBoard(board) {
+  var tempBoardString = "<div class='experiment'>";
+  for (var i = 0; i < board.spaces.length; i++) {
+    tempBoardString += board.spaces[i];
+    if (i === 2 || i === 5) {
+      tempBoardString += "</div><div class='experiment'>";
+    }
+  }
+  tempBoardString += "</div>"
+  $(".board").html(tempBoardString);
+}
+
+
 
 $(document).ready(function() {
   var userType = getUser();
-  alert("you chose " + userType + ", great job");
+  console.log("user type = " + userType);
 
-  displaySpace(buildSpace());
+  //displaySpace(buildSpace(0));
+  placeInSpace(userType, 0);
+  var boardArray = [];
+  var board = new Board(boardArray);
+  board.buildBoard();
+  displayBoard(board);
 });
